f767bf9b225cebbbebddd03c50829b5cbe3fc9f8
Gerald Maboshe <gerald@thezig.io>
2020-05-18 22:19:30 +0200

    code

--- app.js
+++ app.js
@@ -174,3 +174,11 @@ function callSendAPI(sender_psid, response) {
     }
   );
 }
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
